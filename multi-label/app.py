e3907eec6ad8c50152b8e3fa822eeb0487902473
DavideFoini <davide.foini@mail.polimi.it>
2023-05-22 12:00:25 +0200

    added multi-label-head

--- multi-label/app.py
+++ multi-label/app.py
@@ -1,41 +1,18 @@
-from datetime import datetime
-
-import torch
-from torch.utils.data import DataLoader
-
-import datasets.PA100K as PA100K
 from models import *
-from utils import train, test, utils
-from utils.utils import show_labels_accuracy
-
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = MultiTask4()
-model.freeze_backbone()
-model.to(device)
-# utils.model_summary(multitask)
+multitask = torch.load("./results/models/MultiTaskHead.pth")
+head = MultiTaskHead()
 
-dataset = PA100K.Train('./PA-100K', device=device, multitask=True)
-label, gender, pov, sleeve = train.train_multitask4(model, 20, 1e-2, dataset, plot=True, tolerance=1, resume=True, check_path="./multi-label/checkpoints/05182023-195520 MultiTaskV3_checkpoint10left.pt")
+head.label = multitask.label
+head.gender = multitask.gender
+head.gender_conv = multitask.gender_conv
+head.sleeve = multitask.sleeve
+head.sleeve_conv = multitask.sleeve_conv
+head.pov = multitask.pov
+head.pov_conv = multitask.pov_conv
+head.avg_pool =  multitask.avg_pool
 
-model.label = label
-model.gender = gender
-model.pov = pov
-model.sleeve = sleeve
+torch.save(head, "./results/models/multi-label-head.pth")
 
-# label, gender, pov, sleeve = train.train_multitask4(model, 20, 1e-3, dataset, plot=True, tolerance=10, of_label=False)
-#
-# model.label = label
-# model.gender = gender
-# model.pov = pov
-# model.sleeve = sleeve
-
-dataset = PA100K.Test('./PA-100K', device=device, multitask=True)
-test_acc, labels_acc = test.test_multitask(model, dataset)
-show_labels_accuracy(labels_acc, dataset.labels_list, export=True, des="Multitask Genderconv")
-
-
-# now = datetime.now()
-# name = model.name + "" + now.strftime(" %m%d%Y-%H%M%S")
-# torch.save(model, "./multi-label/results/models/{} - {}.pth".format(name, test_acc))
 
--- multi-label/models.py
+++ multi-label/models.py
@@ -112,195 +112,8 @@ class BatchNorm(torch.nn.Module):
         self.head[4] = nn.Linear(in_features=32, out_features=num_classes)
 
 class MultiTask(torch.nn.Module):
-    def __init__(self, load=False, path=None, num_classes=25):
-        super(MultiTask, self).__init__()
-        if load:
-            self.backbone = torch.load(path)
-        else:
-            self.backbone = yolox_nano().backbone.backbone
-
-        self.avg_pool =  nn.AdaptiveAvgPool2d(1)
-
-        self.fc = nn.Sequential(
-            nn.Linear(in_features=256, out_features=256),
-            nn.ReLU()
-        )
-
-        self.gender = nn.Sequential(nn.Linear(in_features=256, out_features=512),
-                                    nn.ReLU(),
-                                    nn.Linear(in_features=512, out_features=1))
-
-        self.label = nn.Linear(in_features=256, out_features=num_classes)
-
-        self.name = "MultiTask"
-
-    def forward(self, x):
-        x = self.backbone(x)[self.backbone.out_features[2]]
-        x = self.avg_pool(x)
-        x = x.squeeze(axis=(2,3))
-        x = self.fc(x)
-        gender = self.gender(x)
-        label =  self.label(x)
-        return gender, label
-
-    def freeze_backbone(self):
-        for param in self.backbone.parameters(): param.requires_grad = False
-
-    def freeze_labels_head(self):
-        for param in self.label.parameters(): param.requires_grad = False
-
-    def change_num_classes(self, num_classes):
-        self.head[4] = nn.Linear(in_features=32, out_features=num_classes)
-
-class MultiTask2(torch.nn.Module):
-    def __init__(self, load=False, path=None, num_classes=25):
-        super(MultiTask2, self).__init__()
-        if load:
-            self.backbone = torch.load(path)
-        else:
-            self.backbone = yolox_nano().backbone.backbone
-
-        self.avg_pool =  nn.AdaptiveAvgPool2d(1)
-
-        self.gender_conv = nn.Sequential(
-            nn.Conv2d(64, 128, 3),
-            nn.ReLU(),
-            nn.MaxPool2d(3),
-            nn.Conv2d(128, 256, 3),
-            nn.ReLU(),
-            nn.MaxPool2d(3)
-        )
-
-        self.gender = nn.Sequential(
-            nn.Linear(in_features=256, out_features=512),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(in_features=512, out_features=512),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(in_features=512, out_features=256),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(in_features=256, out_features=1)
-        )
-
-        self.label = nn.Linear(in_features=64, out_features=num_classes)
-
-        self.name = "MultiTaskV2"
-
-    def forward(self, x):
-        back_out = self.backbone(x)[self.backbone.out_features[0]]
-        label_in = self.avg_pool(back_out)
-        label_in = label_in.squeeze(axis=(2,3))
-        label_out = self.label(label_in)
-
-        gender_out = self.gender_conv(back_out)
-        gender_out = self.avg_pool(gender_out)
-        gender_out = gender_out.squeeze(axis=(2, 3))
-        gender_out = self.gender(gender_out)
-
-        return gender_out, label_out
-
-    def freeze_backbone(self):
-        for param in self.backbone.parameters(): param.requires_grad = False
-
-    def freeze_labels_head(self):
-        for param in self.label.parameters(): param.requires_grad = False
-
-    def change_num_classes(self, num_classes):
-        self.head[4] = nn.Linear(in_features=32, out_features=num_classes)
-
-class MultiTask3(torch.nn.Module):
-    def __init__(self, load=False, path=None, num_classes=22):
-        super(MultiTask3, self).__init__()
-        if load:
-            self.backbone = torch.load(path)
-        else:
-            self.backbone = yolox_nano().backbone.backbone
-
-        self.avg_pool =  nn.AdaptiveAvgPool2d(1)
-
-        self.gender_conv = nn.Sequential(
-            nn.Conv2d(64, 128, 3),
-            nn.ReLU(),
-            nn.MaxPool2d(3),
-            nn.Conv2d(128, 256, 3),
-            nn.ReLU(),
-            nn.MaxPool2d(3)
-        )
-
-        self.gender = nn.Sequential(
-            nn.Linear(in_features=256, out_features=512),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(in_features=512, out_features=512),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(in_features=512, out_features=256),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(in_features=256, out_features=1)
-        )
-
-        self.pov_conv = nn.Sequential(
-            nn.Conv2d(64, 128, 3),
-            nn.ReLU(),
-            nn.MaxPool2d(3),
-            nn.Conv2d(128, 256, 3),
-            nn.ReLU(),
-            nn.MaxPool2d(3)
-        )
-
-        self.pov = nn.Sequential(
-            nn.Linear(in_features=256, out_features=512),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(in_features=512, out_features=512),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(in_features=512, out_features=256),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(in_features=256, out_features=3)
-        )
-
-        self.label = nn.Linear(in_features=64, out_features=num_classes)
-
-        self.name = "MultiTaskV3"
-
-    def forward(self, x):
-        back_out = self.backbone(x)[self.backbone.out_features[0]]
-        pool_out = self.avg_pool(back_out)
-        pool_out = pool_out.squeeze(axis=(2,3))
-        label_out = self.label(pool_out)
-
-        gender_out = self.gender_conv(back_out)
-        gender_out = self.avg_pool(gender_out)
-        gender_out = gender_out.squeeze(axis=(2, 3))
-        gender_out = self.gender(gender_out)
-
-        pov_out = self.pov_conv(back_out)
-        pov_out = self.avg_pool(pov_out)
-        pov_out = pov_out.squeeze(axis=(2, 3))
-        pov_out = self.pov(pov_out)
-
-        return gender_out, pov_out, label_out
-
-    def freeze_backbone(self):
-        for param in self.backbone.parameters(): param.requires_grad = False
-
-    def freeze_labels_head(self):
-        for param in self.label.parameters(): param.requires_grad = False
-
-    def freeze_pov_head(self):
-        for param in self.pov.parameters(): param.requires_grad = False
-
-    def change_num_classes(self, num_classes):
-        self.head[4] = nn.Linear(in_features=32, out_features=num_classes)
-
-class MultiTask4(torch.nn.Module):
     def __init__(self, load=False, path=None, num_classes=20):
-        super(MultiTask4, self).__init__()
+        super(MultiTask, self).__init__()
         if load:
             self.backbone = torch.load(path)
         else:
@@ -392,6 +205,100 @@ class MultiTask4(torch.nn.Module):
 
     def freeze_backbone(self):
         for param in self.backbone.parameters(): param.requires_grad = False
+
+    def freeze_labels_head(self):
+        for param in self.label.parameters(): param.requires_grad = False
+
+    def freeze_pov_head(self):
+        for param in self.pov.parameters(): param.requires_grad = False
+
+    def change_num_classes(self, num_classes):
+        self.head[4] = nn.Linear(in_features=32, out_features=num_classes)
+
+class MultiTaskHead(torch.nn.Module):
+    def __init__(self, num_classes=20):
+        super(MultiTaskHead, self).__init__()
+        self.avg_pool =  nn.AdaptiveAvgPool2d(1)
+        self.gender_conv = nn.Sequential(
+            nn.Conv2d(64, 128, 3),
+            nn.ReLU(),
+            nn.MaxPool2d(3),
+            nn.Conv2d(128, 256, 3),
+            nn.ReLU(),
+            nn.MaxPool2d(3)
+        )
+
+        self.gender = nn.Sequential(
+            nn.Linear(in_features=256, out_features=512),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(in_features=512, out_features=512),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(in_features=512, out_features=256),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(in_features=256, out_features=1)
+        )
+
+        self.pov_conv = nn.Sequential(
+            nn.Conv2d(64, 128, 3),
+            nn.ReLU(),
+            nn.MaxPool2d(3),
+            nn.Conv2d(128, 256, 3),
+            nn.ReLU(),
+            nn.MaxPool2d(3)
+        )
+
+        self.pov = nn.Sequential(
+            nn.Linear(in_features=256, out_features=512),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(in_features=512, out_features=512),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(in_features=512, out_features=256),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(in_features=256, out_features=3)
+        )
+
+        self.sleeve_conv = nn.Sequential(
+            nn.Conv2d(64, 128, 3),
+            nn.ReLU(),
+            nn.MaxPool2d(3),
+            nn.Conv2d(128, 256, 3),
+            nn.ReLU(),
+            nn.MaxPool2d(3)
+        )
+
+        self.sleeve = nn.Linear(in_features=256, out_features=2)
+
+        self.label = nn.Linear(in_features=64, out_features=num_classes)
+
+        self.name = "MultiTaskHead"
+
+    def forward(self, x):
+        pool_out = self.avg_pool(x)
+        pool_out = pool_out.squeeze(axis=(2,3))
+        label_out = self.label(pool_out)
+
+        gender_out = self.gender_conv(x)
+        gender_out = self.avg_pool(gender_out)
+        gender_out = gender_out.squeeze(axis=(2, 3))
+        gender_out = self.gender(gender_out)
+
+        pov_out = self.pov_conv(x)
+        pov_out = self.avg_pool(pov_out)
+        pov_out = pov_out.squeeze(axis=(2, 3))
+        pov_out = self.pov(pov_out)
+
+        sleeve_out = self.sleeve_conv(x)
+        sleeve_out = self.avg_pool(sleeve_out)
+        sleeve_out = sleeve_out.squeeze(axis=(2, 3))
+        sleeve_out = self.sleeve(sleeve_out)
+
+        return gender_out, pov_out, sleeve_out, label_out
 
     def freeze_labels_head(self):
         for param in self.label.parameters(): param.requires_grad = False
